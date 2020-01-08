712760575cb307ec45ef4b68596d5a7944260654
Imad Elakhal <imadelakhaldev@gmail.com>
2020-01-08 16:38:29 +0100

    Ananimal buttons added

---
+++ animal_buttons/script.js
@@ -0,0 +1,17 @@
+var bg = document.querySelector("body");
+
+document.getElementById('dog').addEventListener('click', function(){
+  bg.style.backgroundImage = "url('https://media.giphy.com/media/138clYUZi54SoE/giphy.gif')";
+});
+document.getElementById('cat').addEventListener('click', function(){
+  bg.style.backgroundImage = "url('https://media.giphy.com/media/61tFzCr556Wsg/giphy.gif')";
+})
+document.getElementById('fish').addEventListener('click', function(){
+  bg.style.backgroundImage = "url('https://media.giphy.com/media/2UtliQZm3mnT6Tea4b/giphy.gif')";
+});
+document.getElementById('spider').addEventListener('click', function(){
+  bg.style.backgroundImage = "url('https://media.giphy.com/media/oXnN2TNSgfJQI/giphy.gif')";
+});
+document.getElementById('frog').addEventListener('click', function(){
+  bg.style.backgroundImage = "url('https://media.giphy.com/media/T2Rx0jWVSYttC/giphy.gif')";
+});
